491adf30f0a1d60575263f2261e53b68639641df
mathias frommelt <frommelt@live.dk>
2024-04-17 11:52:52 +0200

    Created frontend and integrated it withthe backend for animal search and filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
 import "./App.css";
+import { useState } from "react";
+
+// Define the Animal interface
+interface Animal {
+  id: number;
+  name: string;
+  type: string;
+  age: number;
+}
 
 function App() {
-  return <div></div>;
+  const [animals, setAnimals] = useState<Animal[]>([]);
+
+  const search = async (q: string) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+    // print the data to the console
+    console.log(data);
+    setAnimals(data);
+  };
+
+  return (
+    <main>
+      <h1>Animal Farm Search</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
+}
+
+function Animal({ name, type, age }: Animal) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} {age}
+    </li>
+  );
 }
 
 export default App;
